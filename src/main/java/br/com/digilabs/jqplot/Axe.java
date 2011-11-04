8c229d4767c7e59656d1d1237d8377c57b635e76
Inaiat Henrique <inaiat@gmail.com>
2011-11-04 20:32:13 -0200

    first commit

--- src/main/java/br/com/digilabs/jqplot/Axe.java
+++ src/main/java/br/com/digilabs/jqplot/Axe.java
@@ -18,5 +18,75 @@ public class Axe implements Serializable {
     private boolean autoScale;
     private Collection<String> tickOptions;
     private Collection<String> ticks;
+
+    /**
+     * @return the show
+     */
+    public boolean isShow() {
+        return show;
+    }
+
+    /**
+     * @param show the show to set
+     */
+    public void setShow(boolean show) {
+        this.show = show;
+    }
+
+    /**
+     * @return the renderer
+     */
+    public Renderer getRenderer() {
+        return renderer;
+    }
+
+    /**
+     * @param renderer the renderer to set
+     */
+    public void setRenderer(Renderer renderer) {
+        this.renderer = renderer;
+    }
+
+    /**
+     * @return the autoScale
+     */
+    public boolean isAutoScale() {
+        return autoScale;
+    }
+
+    /**
+     * @param autoScale the autoScale to set
+     */
+    public void setAutoScale(boolean autoScale) {
+        this.autoScale = autoScale;
+    }
+
+    /**
+     * @return the tickOptions
+     */
+    public Collection<String> getTickOptions() {
+        return tickOptions;
+    }
+
+    /**
+     * @param tickOptions the tickOptions to set
+     */
+    public void setTickOptions(Collection<String> tickOptions) {
+        this.tickOptions = tickOptions;
+    }
+
+    /**
+     * @return the ticks
+     */
+    public Collection<String> getTicks() {
+        return ticks;
+    }
+
+    /**
+     * @param ticks the ticks to set
+     */
+    public void setTicks(Collection<String> ticks) {
+        this.ticks = ticks;
+    }
     
 }
