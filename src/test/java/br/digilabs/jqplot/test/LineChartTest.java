c447a9eb3a2dcfefb487777c92ab4867b2b35dc6
Inaiat Henrique <inaiat@gmail.com>
2011-11-18 10:31:22 -0200

    Refactoring

---
+++ src/test/java/br/digilabs/jqplot/test/LineChartTest.java
@@ -0,0 +1,43 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.digilabs.jqplot.test;
+
+import br.digilabs.jqplot.JqPlotUtil;
+import br.digilabs.jqplot.chart.LineChart;
+import br.digilabs.jqplot.chart.data.LinedData;
+import junit.framework.TestCase;
+
+/**
+ *
+ * @author inaiat
+ */
+public class LineChartTest extends TestCase {
+
+    public LineChartTest(String testName) {
+        super(testName);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    /**
+     * Test of createJquery method, of class JqPlotUtil.
+     */
+    public void testCreateJquery() {       
+        
+        LineChart<LinedData<Integer>> lineChart = new LineChart<LinedData<Integer>>();
+        LinedData<Integer> data = new LinedData<Integer>(1, 2, 3, 4, 5, 6);
+        lineChart.setChartData(data);            
+        System.out.println(JqPlotUtil.createJquery(lineChart, "chart1"));
+    }
+
+}
