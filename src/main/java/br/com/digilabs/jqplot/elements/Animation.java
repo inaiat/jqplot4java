6204b103f2ea91ac9d2545be1df0040bdc460ccb
Serban Balamaci <balamaci@gmail.com>
2012-03-12 23:21:07 +0200

    animation property

---
+++ src/main/java/br/com/digilabs/jqplot/elements/Animation.java
@@ -0,0 +1,18 @@
+package br.com.digilabs.jqplot.elements;
+
+/**
+ * User: Serban Balamaci
+ * Date: 3/12/12
+ */
+public class Animation implements Element {
+
+    private Integer speed;
+
+    public Integer getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(Integer speed) {
+        this.speed = speed;
+    }
+}
