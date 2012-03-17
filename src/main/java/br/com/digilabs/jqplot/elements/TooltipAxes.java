b137b116e17a32eb57d98db86d96958c40ebbf5d
Serban Balamaci <balamaci@gmail.com>
2012-03-17 12:36:05 +0200

    tooltip location enums

---
+++ src/main/java/br/com/digilabs/jqplot/elements/TooltipAxes.java
@@ -0,0 +1,13 @@
+package br.com.digilabs.jqplot.elements;
+
+/**
+ * @author balamaci
+ */
+public enum TooltipAxes {
+
+    x,
+    y,
+    both,
+    xy,
+    yx
+}
---
+++ src/main/java/br/com/digilabs/jqplot/elements/TooltipLocation.java
@@ -0,0 +1,17 @@
+package br.com.digilabs.jqplot.elements;
+
+/**
+ * @author balamaci
+ */
+public enum TooltipLocation {
+
+    n,
+    ne,
+    e,
+    se,
+    s,
+    sw,
+    w,
+    nw
+
+}
